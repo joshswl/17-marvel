d09686e50fc3c8222e64defae84c0af3077445ad
Josh Lee <joshswl@gmail.com>
2017-03-02 16:55:34 -0600

    almost done with styling

--- app/components/application.vue
+++ app/components/application.vue
@@ -16,7 +16,7 @@
                 <p class="content__sidebar--years">2010 - 2011</p>
               </div>
               <div class="content__sidebar--creators">
-                <h3 class="content__sidebar--creators__title">Creators</h3>
+                <h3 class="title">Creators</h3>
                 <ul class="content__sidebar--creators__list">
                   <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
                   <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
@@ -28,8 +28,60 @@
               </div>
             </div>
             <div class="content__main">
-              <div class="content__main--characters"></div>
-              <div class="content__main--comics"></div>
+              <div class="content__main--characters">
+                <h2 class="title">Characters</h2>
+                <ul class="content__main--grid">
+                  <li class="content__main--grid__item">
+                    <img src="http://fillmurray.com/90/90" alt="" class="content__main--grid__item--image">
+                    <p class="content__main--grid__item--text">Placeholder Character</p>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://fillmurray.com/90/90" alt="" class="content__main--grid__item--image">
+                    <p class="content__main--grid__item--text">Placeholder Character</p>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://fillmurray.com/90/90" alt="" class="content__main--grid__item--image">
+                    <p class="content__main--grid__item--text">Placeholder Character</p>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://fillmurray.com/90/90" alt="" class="content__main--grid__item--image">
+                    <p class="content__main--grid__item--text">Placeholder Character</p>
+                  </li>
+                </ul>
+              </div>
+              <div class="content__main--comics">
+                <h2 class="title">Comics</h2>
+                <ul class="content__main--grid">
+                  <li class="content__main--grid__item">
+                    <img src="http://lorempixel.com/90/90" alt="">
+                    <div class="content__main--grid__item--text">
+                      <p class="content__main--grid__item--number">#1</p>
+                      <p class="content__main--grid__item--name">Placeholder Comic (2010) #1</p>
+                    </div>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://lorempixel.com/90/90" alt="">
+                    <div class="content__main--grid__item--text">
+                      <p class="content__main--grid__item--number">#2</p>
+                      <p class="content__main--grid__item--name">Placeholder Comic (2010) #2</p>
+                    </div>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://lorempixel.com/90/90" alt="">
+                    <div class="content__main--grid__item--text">
+                      <p class="content__main--grid__item--number">#3</p>
+                      <p class="content__main--grid__item--name">Placeholder Comic (2010) #3</p>
+                    </div>
+                  </li>
+                  <li class="content__main--grid__item">
+                    <img src="http://lorempixel.com/90/90" alt="">
+                    <div class="content__main--grid__item--text">
+                      <p class="content__main--grid__item--number">#4</p>
+                      <p class="content__main--grid__item--name">Placeholder Comic (2010) #4</p>
+                    </div>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
