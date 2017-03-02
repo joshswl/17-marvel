883abc3074a97b32818c21ed73aa6d7b68cb3680
Josh Lee <joshswl@gmail.com>
2017-03-02 14:51:58 -0600

    top nav set

---
+++ app/components/application.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="">
+    <div class="app">
+      <div class="section">
+        <div class="container">
+          <div class="top-nav">
+            <div class="top-nav__text">
+              <p class="top-nav__text--actual">Marvel</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+
+  methods: {
+
+  },
+};
+</script>
