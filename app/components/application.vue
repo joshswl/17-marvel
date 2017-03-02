83d1b7c63966cd5b34efb53f9c12690a71bf005d
Josh Lee <joshswl@gmail.com>
2017-03-02 15:54:46 -0600

    left bar stylized

--- app/components/application.vue
+++ app/components/application.vue
@@ -5,7 +5,31 @@
         <div class="container">
           <div class="top-nav">
             <div class="top-nav__text">
-              <p class="top-nav__text--actual">Marvel</p>
+              <h2 class="top-nav__text--actual">Marvel</h2>
+            </div>
+          </div>
+          <div class="content">
+            <div class="content__sidebar">
+              <div class="content__sidebar--character">
+                <img src="http://placecage.com/220/220" alt="" class="content__sidebar--photo">
+                <h3 class="content__sidebar--title">Spider-Girl (2010 - 2011)</h3>
+                <p class="content__sidebar--years">2010 - 2011</p>
+              </div>
+              <div class="content__sidebar--creators">
+                <h3 class="content__sidebar--creators__title">Creators</h3>
+                <ul class="content__sidebar--creators__list">
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                  <li class="content__sidebar--creators__list--item">Creator Placeholder</li>
+                </ul>
+              </div>
+            </div>
+            <div class="content__main">
+              <div class="content__main--characters"></div>
+              <div class="content__main--comics"></div>
             </div>
           </div>
         </div>
